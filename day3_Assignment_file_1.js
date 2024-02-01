0cc22a8f137efb7783680b2ef3d79c53a57e6e95
siddhantnilange-coditas <158148549+siddhantnilange-coditas@users.noreply.github.com>
2024-02-01 08:55:45 +0530

    Add files via upload

---
+++ day3_Assignment_file_1.js
@@ -0,0 +1,39 @@
+
+
+let strSentence1="This is string";
+console.log(strSentence1);
+let strSentence2='String in single quotes';
+console.log(strSentence2);
+
+let nDenoter=null;
+console.log(nDenoter);
+let defDenoter=undefined
+console.log(defDenoter);
+const constUndef=undefined;
+console.log(constUndef);
+const nullUndef=null;
+console.log(nullUndef);
+
+
+const eltp = ["Siddhant", "Suyash", "Annirudh", "Akhil", "Arjun"];
+console.log(eltp);
+
+const exampleArray = ["Siddhant", 22, "Saish", 23];
+console.log(exampleArray);
+
+const dataObj = {firstName:"Siddhant", lastName:"Nilange", age:22};
+console.log(dataObj);
+
+
+let intBigint =BigInt("3223434709889007800232343");
+console.log(intBigint);
+
+let smallInt= SmallInt("-8430848404858408404494040484")
+console.log(smallInt);
+
+let boolVar1 = false
+console.log(boolVar1);
+;
+let boolVar2 = new Boolean(false);
+console.log(boolVar2);
+
---
+++ day3_Assignment_file_2.js
@@ -0,0 +1,56 @@
+
+console.log(typeof null);
+
+console.log(typeof NaN);
+
+
+let numVar=100;
+console.log(typeof numVar);
+let floatVar=100.100;
+console.log(typeof floatVar);
+let nullVar=null;
+console.log(typeof nullVar);
+let boolVar=true;
+console.log(typeof boolVar);
+let notANumber=NaN;
+console.log(typeof notANumber);
+
+let strSentence1="This is string";
+console.log(typeof(strSentence1));
+let strSentence2='String in single quotes';
+console.log(typeof(strSentence2));
+let strSentence3="Sting with double and 'single' quotes";
+console.log(typeof(strSentence3));
+
+let nDenoter=null;
+console.log(typeof(nDenoter));
+let defDenoter=undefined
+console.log(typeof(defDenoter));
+const constUndef=undefined;
+console.log(typeof(constUndef));
+const nullUndef=null;
+console.log(typeof(nullUndef));
+
+
+const eltp = ["Siddhant", "Suyash", "Annirudh", "Akhil", "Arjun"];
+console.log(typeof(eltp));
+
+const exampleArray = ["Siddhant", 22, "Saish", 23];
+console.log(typeof(exampleArray));
+
+const dataObj = {firstName:"Siddhant", lastName:"Nilange", age:22};
+console.log(typeof(dataObj));
+
+
+let intBigint =BigInt("3223434709889007800232343");
+console.log(typeof(intBigint));
+
+
+let boolVar1 = false
+console.log(typeof(boolVar1));
+
+let boolVar2 = new Boolean(false);
+console.log(typeof(boolVar2));
+
+
+
